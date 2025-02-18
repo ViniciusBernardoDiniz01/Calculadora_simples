8a622bf4fdd89b68d4a62e6508664f70c983837a
Vinicius <vinicius.diniz.senai404@gmail.com>
2025-02-18 16:24:40 -0300

    Add files via upload

---
+++ Atividade Dom/Atividade1.js
@@ -0,0 +1,35 @@
+function calcular(){
+    let numero1 = parseFloat(document.getElementById("numero1").value);
+    let numero2 = parseFloat(document.getElementById("numero2").value);
+    let operador = String(document.getElementById("operadores").value);
+
+    switch(operador){
+        case "Soma":
+            let soma = numero1 + numero2;
+            document.getElementById("texto").innerText = "A soma é: "+soma;
+            break;
+        case "Subtracao":
+            let Subtrai = numero1 - numero2;
+            document.getElementById("texto").innerText = "A subtração é: "+Subtrai;
+            break;
+        case "divisao":
+            let divide = numero1 / numero2;
+            document.getElementById("texto").innerText = "A divisão é: "+divide;
+            break;
+        case "multiplicacao":
+            let mult = numero1 * numero2;
+            document.getElementById("texto").innerText = "A multiplicação é: "+mult;
+            break;
+        case "potencia":
+            let poten = Math.pow(numero1, numero2);
+            document.getElementById("texto").innerText = "A potencia é: "+poten;
+            break;
+        case "porcentagem":
+            let porcen = (numero1*numero2) / 100;
+            document.getElementById("texto").innerText = "A porcentagem é: "+porcen;
+            break;
+        default:
+            document.getElementById("texto").innerText = "ERRO Escolha um Operador Logico";
+    }
+}
+
